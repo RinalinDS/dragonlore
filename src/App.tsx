b34b64b92894d6ac76d572c468e6c5105ed204ce
DenisPilyutin <denispilyutin@gmail.com>
2024-02-16 03:46:27 +0200

    feat: add getPosts request

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { useGetPosts } from './api/queries/useGetPosts';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export const App = () => {
-  return <div>hello</div>;
+  const { data = [], isFetching, isLoading } = useGetPosts({});
+
+  return (
+    <div>
+      {isLoading || isFetching ? (
+        <div>...Posts Loading</div>
+      ) : (
+        data.map((post) => {
+          return (
+            <div key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          );
+        })
+      )}
+      <ToastContainer />
+    </div>
+  );
 };
---
+++ src/api/queries/useGetPosts.ts
@@ -0,0 +1,33 @@
+import { useQuery } from '@tanstack/react-query';
+import { instance } from '../config';
+import { queryKeys } from '../queryKeys';
+import { toast } from 'react-toastify';
+
+type Request = {
+  page?: number;
+  itemsPerPage?: number;
+};
+
+export type Post = {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+};
+
+const getPosts = async ({ page, itemsPerPage }: Request): Promise<Post[]> => {
+  const response = await instance.get(
+    `posts?_start=${page}&_limit=${itemsPerPage}`
+  );
+  toast('data was loaded');
+  return response.data;
+};
+
+export const useGetPosts = ({ page = 0, itemsPerPage = 10 }: Request) => {
+  const { data, isLoading, isFetching } = useQuery({
+    queryKey: [queryKeys.posts, page, itemsPerPage],
+    queryFn: () => getPosts({ itemsPerPage, page }),
+  });
+
+  return { data, isFetching, isLoading };
+};
